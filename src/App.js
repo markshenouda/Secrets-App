0ba5ee2b86e087d3eaa2b56363305b6529573da1
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2021-04-27 17:32:36 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.png";
+import React, { useState } from "react";
+const CryptoJS = require("crypto-js");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [message, messageSet] = useState("");
+	const [key, keySet] = useState("");
+	const [result, resultSet] = useState("");
+
+	function encrypt() {
+		resultSet(CryptoJS.AES.encrypt(message, key).toString());
+	}
+
+	function decrypt() {
+		let d = CryptoJS.AES.decrypt(message, key);
+		resultSet(d.toString(CryptoJS.enc.Utf8));
+	}
+
+	return (
+		<div className="app">
+			<img src={logo} alt="" className="logo" />
+			<textarea
+				className="input-text"
+				rows="15"
+				value={message}
+				onChange={(e) => messageSet(e.target.value)}
+				placeholder="Type your text here..."
+			></textarea>
+			<input
+				type="text"
+				value={key}
+				onChange={(e) => keySet(e.target.value)}
+				className="input-text"
+				placeholder="Encryption Key..."
+			/>
+			<div className="control-buttons">
+				<button onClick={encrypt}>ENCRYPT</button>
+				<button onClick={decrypt}>DECRYPT</button>
+			</div>
+			<h1 className="result-title">Result</h1>
+			<div
+				className="input-text"
+				style={{ minHeight: "300px", wordBreak: "break-all" }}
+			>
+				{result}
+			</div>
+			<div className="footer">
+				Made with 💚 by{" "}
+				<a
+					href="https://twitter.com/MarkSShenouda"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Mark Shenouda
+				</a>
+			</div>
+		</div>
+	);
 }
 
 export default App;
